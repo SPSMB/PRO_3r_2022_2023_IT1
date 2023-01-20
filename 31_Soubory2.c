c56ad6011b4be2f3274842691333d7d5ed33262d
zimmerhakl <tomas.zimmerhakl@gmail.com>
2023-01-20 21:15:29 +0100

    20.1. - Soubory - čtení ze souboru

--- 31_Soubory2.c
+++ 31_Soubory2.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #pragma warning(disable:4996)
 
 int readline( char * buf, int max, FILE * f ){
@@ -18,21 +20,25 @@ int readline( char * buf, int max, FILE * f ){
 
 int main(int argc, char const *argv[])
 {
-	const char * nazevSouboru = "30_soubor1.txt";
+	const char * nazevSouboru = "30_soubor3.txt";
 	FILE * soubor = fopen(nazevSouboru, "r");
 	printf("Oteviram soubor %s.\n", nazevSouboru);
 
-	// int a = 0;
+	if(soubor == NULL){
+		int chyba = errno;
+		printf("Soubor %s se nepodarilo otevrit.\nCislo chyby: %d\nHlaska: %s\n", nazevSouboru, chyba, strerror(chyba));
+		return 0;
+	}
+
 	char text[150];
-/*
-	do {
-		fscanf(soubor, "%d", &a);
-		printf("Nacetl jsem %d\n", a);
-	} while (a != 0);
-*/
+
 	readline(text,150,soubor);
-	//fgets(text, 150, soubor);
-	printf(">%s<",text);
+	printf(">%s<\n",text);
+	
+	fseek(soubor, 2, SEEK_SET);
+	readline(text,150,soubor);
+	printf(">%s<\n",text);
+
 	fclose(soubor);
 	return 0;
 }
