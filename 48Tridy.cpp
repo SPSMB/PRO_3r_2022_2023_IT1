25680c3438156d9ef02164b72cfe830af2eee69e
zimmerhakl <tomas.zimmerhakl@gmail.com>
2023-06-23 14:44:00 +0200

    23.6. - Jazyk C++ - Třída Obdélník - funce pro otočení obdélníku podle libovolného středu

--- 48Tridy.cpp
+++ 48Tridy.cpp
@@ -1,13 +1,14 @@
 #include <string>
 #include <cstdio>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 class Obdelnik {
 private:
-	int o_x;
-	int o_y;
+	int o_ax; // levy dolni roh
+	int o_ay; // levy dolni roh
 	int o_vyska;
 	int o_sirka;
 
@@ -20,10 +21,15 @@ public:
 		return o_sirka;
 	}
 	void nastavRozmery(int x, int y){
-		o_x = x;
-		o_y = y;
+		o_ax = x;
+		o_ay = y;
 	}
-	void otoc(int x, int y, char smer);
+	void otoc(int x, int y, int alpha);
+	void transformace(int vstup_x, int vstup_y, int * vystup_x, int * vystup_y, int s_x, int s_y);
+	void otocPodleStredu(int vstup_x, int vstup_y, int * vystup_x, int * vystup_y, int alpha);
+	void zpetnatransformace(int vstup_x, int vstup_y, int * vystup_x, int * vystup_y, int s_x, int s_y);
+	void normalizace(int a_x, int a_y, int b_x, int b_y);
+
 	bool jeUvnitr(int x, int y);
 	bool jeCtverec();
 	void vypis();
@@ -37,8 +43,8 @@ Obdelnik::Obdelnik(){
 }
 */
 Obdelnik::Obdelnik(int x = 0, int y = 0, int sirka = 0, int vyska = 0){
-	o_x = x;
-	o_y = y;
+	o_ax = x;
+	o_ay = y;
 	o_sirka = sirka;
 	o_vyska = vyska;
 }
@@ -47,9 +53,70 @@ Obdelnik::~Obdelnik(){
 	cout << "Konec objektu" << endl;
 }
 
+void Obdelnik::transformace(int vstup_x, int vstup_y, int * vystup_x, int * vystup_y, int s_x, int s_y){
+	*vystup_x = vstup_x - s_x;
+	*vystup_y = vstup_y - s_y;
+}
+
+void Obdelnik::zpetnatransformace(int vstup_x, int vstup_y, int * vystup_x, int * vystup_y, int s_x, int s_y){
+	*vystup_x = vstup_x + s_x;
+	*vystup_y = vstup_y + s_y;
+}
+
+void Obdelnik::otocPodleStredu(int vstup_x, int vstup_y, int * vystup_x, int * vystup_y, int alpha){
+	double pi = 2*acos(0.0);
+	double alphaRad = alpha*(pi/180.0);
+	*vystup_x = vstup_x*cos(alphaRad) - vstup_y*sin(alphaRad);
+	*vystup_y = vstup_x*sin(alphaRad) + vstup_y*cos(alphaRad);
+}
+
+void Obdelnik::normalizace(int a_x, int a_y, int b_x, int b_y){
+	if(a_x < b_x){
+		o_ax = a_x;
+	} else {
+		o_ax = b_x;
+	}
+	if(a_y < b_y){
+		o_ay = a_y;
+	} else {
+		o_ay = b_y;
+	}
+}
+
+// otoci obdelnik podle libovolneho stredu o 90,-90,180 stupnu
+void Obdelnik::otoc(int x, int y, int alpha){
+	
+	int ax = o_ax;
+	int ay = o_ay;
+	int bx = o_ax + o_sirka;
+	int by = o_ay + o_vyska;
+
+	// 1) transformace
+	transformace(ax, ay, &ax, &ay, x, y);
+	transformace(bx, by, &bx, &by, x, y);
+
+	// 2) otoceni
+	otocPodleStredu(ax, ay, &ax, &ay, alpha);
+	otocPodleStredu(bx, by, &bx, &by, alpha);
+	
+	// 3) zpetna transformace
+	zpetnatransformace(ax, ay, &ax, &ay, x, y);
+	zpetnatransformace(bx, by, &bx, &by, x, y);
+	
+	// 4) normalizace
+	normalizace(ax, ay, bx, by);
+	// srovnam sirku a vysku
+	if(alpha == 90 || alpha == -90){
+		int tmpSirka;
+		tmpSirka = o_sirka;
+		o_sirka = o_vyska;
+		o_vyska = tmpSirka; 
+	}
+}
+
 bool Obdelnik::jeUvnitr(int x, int y){
-	if(  o_x <= x && x <= o_x + o_sirka &&
-		 o_y <= y && y <= o_y + o_vyska){
+	if(  o_ax <= x && x <= o_ax + o_sirka &&
+		 o_ay <= y && y <= o_ay + o_vyska){
 		return true;
 	} else {
 		return false;
@@ -65,17 +132,20 @@ bool Obdelnik::jeCtverec(){
 }
 
 void Obdelnik::vypis(){
-	cout << "Obdelnik na souradnicich [" << o_x << "," << o_y 
+	cout << "Obdelnik na souradnicich [" << o_ax << "," << o_ay 
 	<< "] sirka:" << o_sirka << ", vyska:" << o_vyska << endl;
 }
 
 int main(){
 
-	Obdelnik silnice1;
-	Obdelnik silnice2(2,15);
+	Obdelnik o1(2,3,4,2);
+	o1.vypis();
+	o1.otoc(4,6,180);
+	o1.vypis();
 
-	cout << "Sirka silnice je " << silnice2.sirka() << endl;
+	
 
+/*
 	silnice2.vypis();
 	if(silnice2.jeCtverec()){
 		cout << "je ctverec" << endl;
@@ -88,7 +158,7 @@ int main(){
 	} else {
 		cout << "Bod je venku\n";
 	}
-
+*/
 
 	return 0;
 }
